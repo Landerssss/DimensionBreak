777be512176c4e965e6a9fd0b20b719c35628db0
Landers.A <1608012376@qq.com>
2025-12-22 00:05:55 +0800

    修复如果在 Die() 方法里启动了协程，调用 Destroy，对象会被立即销毁它的协程也会停止。结果导致游戏0.1 倍速没有办法恢复

--- Assets/Scrips/LEVEL1_2D/SimpleEnemy.cs
+++ Assets/Scrips/LEVEL1_2D/SimpleEnemy.cs
@@ -21,16 +21,17 @@ public class SimpleEnemy : MonoBehaviour
 
     void Die()
     {
-        // 1. 生成特效
+        // 1. 生成特效 (在销毁前生成)
         if (deathEffectPrefab != null)
         {
             Instantiate(deathEffectPrefab, transform.position, Quaternion.identity);
         }
-        //砍中怪的瞬间画面卡顿一下，简直是太爽了！
+
+        // 2. 启动顿帧协程
+        // 关键点：不要在这里直接调用 Destroy(gameObject)，否则协程会中断，游戏会一直卡在慢动作！
         StartCoroutine(HitStop());
-        // 2. 销毁自身
-        Destroy(gameObject);
     }
+    // --- 新增的顿帧逻辑 ---
     IEnumerator HitStop()
     {
         // 瞬间变慢（模拟卡顿感）
@@ -41,8 +42,11 @@ public class SimpleEnemy : MonoBehaviour
 
         // 恢复正常速度
         Time.timeScale = 1f;
+
+        // 3. 顿帧结束后，再销毁自身
+        Destroy(gameObject);
     }
-        System.Collections.IEnumerator FlashRed()
+    System.Collections.IEnumerator FlashRed()
     {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         if(sr != null) 
